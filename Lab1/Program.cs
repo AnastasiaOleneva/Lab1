b4287b614f35fc26c8bd468734334bcbda833b2a
79162 <79162@LAPTOP-5NFBVRQ6>
2021-10-17 17:28:35 +0300

    Добавьте файлы проекта.

---
+++ Lab1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("Введите число > 0");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int n_2 = 0;
+            for (int i = 1; i <= (2 * n - 1); i += 2)
+
+            {
+
+                n_2 += i;
+
+            }
+
+            Console.WriteLine("Квадрат числа {0} равен {1}", n, n_2);
+            Console.ReadKey();
+
+
+        }
+    }
+}
